3eedd69fef056fed6bbfcdafe5f15347435b8b64
Francisco Souza <fss@corp.globo.com>
2013-10-18 14:51:40 -0300

    install-tsuru: fix commands
    Gandalf and git-daemon start automatically on postinst, we need to
    restart it.

--- install-tsuru.sh
+++ install-tsuru.sh
@@ -50,10 +50,10 @@ host: localhost
 bind: localhost:8000
 uid: git
 EOF
-start gandalf-server
+restart gandalf-server
 
 echo Starting git-daemon
-start git-daemon
+restart git-daemon
 
 echo Configuring and starting beanstalkd
 cat > /etc/default/beanstalkd <<EOF
